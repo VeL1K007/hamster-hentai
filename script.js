8fa955efae143fe8a55e85ae16be8f5cbbc4cbd3
VeL1K007 <107301449+VeL1K007@users.noreply.github.com>
2024-06-28 23:58:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-const apiUrl = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=english';
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const hamster = document.getElementById('hamster');
+    const hentaiLinks = [
+        'https://online.hentai-hub.net/browse/random/'
+        // Добавьте сюда ссылки на свои ресурсы
+    ];
 
-async function getRandomHentai() {
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Ошибка HTTP: ' + response.status);
-        }
-        const data = await response.json();
-
-        // Выбираем случайное изображение
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const hentai = data[randomIndex];
-
-        // Отображаем изображение
-        displayHentai(hentai.file_url);
-    } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-    }
-}
-
-function displayHentai(imageUrl) {
-    const imageElement = document.createElement('img');
-    imageElement.src = imageUrl;
-    imageElement.alt = 'Hentai Image'; // Добавляем альтернативный текст для доступности
-
-    const container = document.getElementById('hentai-container'); // Получаем контейнер для изображений
-    container.innerHTML = ''; // Очищаем контейнер от предыдущих изображений (если они есть)
-    container.appendChild(imageElement); // Добавляем изображение в контейнер
-}
-
-// Получаем ссылку на изображение хомяка
-const hamsterImage = document.getElementById('hamster-image');
-
-// Добавляем обработчик события клика на изображение хомяка
-hamsterImage.addEventListener('click', getRandomHentai);
+    hamster.addEventListener('click', function() {
+        const randomIndex = Math.floor(Math.random() * hentaiLinks.length);
+        const randomHentaiLink = hentaiLinks[randomIndex];
+        window.open(randomHentaiLink, '_blank');
+    });
+});
